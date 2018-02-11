e0884517f50118025bd77da1a21321fad0474d47
vic <vic@vics-MacBook-Pro.local>
2018-02-11 11:04:52 -0500

    clear ahead of whitelist git

--- release/LG.sh
+++
@@ -1,6 +0,0 @@
-clear
-#export PATH=$PATH:/root/jdk1.8.0_162/bin
-# needs dat.jar in same dir. also you can > site1.txt
-java -Xmx2800m -XX:+UseStringDeduplication -XX:+OptimizeStringConcat -XX:ThreadStackSize=228k -Xss228k -XX:+UseLargePages -XX:+AggressiveOpts -jar loadGen.jar 
-
-
